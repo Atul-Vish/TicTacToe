7e62b6fad456435bf0f22020268b438a2262c71a
Atul Vishwakarma <atulvishw240@gmail.com>
2024-07-09 13:31:10 +0530

    Create two players

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -2,8 +2,10 @@
 module TicTacToe
 	# It creates a new Game and contain two players and a board
 	class Game
-		@board = Array.new(10) 
-		@players = 
+		def initialize(player_1_class, player_2_class)
+			@board = Array.new(10) 
+			@players = [player_1_class.new(self, "X"), player_2_class.new(self, "O")]
+		end
 	end
 
 	# It creates Player
@@ -12,5 +14,8 @@ module TicTacToe
 			@game = game # Player should know which game he's playing so he knows which board is available to him
 			@marker = marker
 		end
+	end
+
+	class HumanPlayer < Player
 	end
 end
