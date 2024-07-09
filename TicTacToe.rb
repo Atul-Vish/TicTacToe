26e857068cd0f9320e8188d76d56bc79c0244f7f
Atul Vishwakarma <atulvishw240@gmail.com>
2024-07-09 14:38:17 +0530

    Create method for player to select position

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -16,10 +16,16 @@ module TicTacToe
 			rows_for_display = row_positions.map(&row_for_display)
 			puts rows_for_display.join("\n" + row_separator + "\n")
 		end
+
+		# Return the free positions on the board, in the form of array
+		def free_positions
+			(1..9).select {|position| @board[position].nil?}
+		end
 	end
 
 	# It creates Player
 	class Player
+		attr_accessor :marker
 		def initialize(game, marker)
 			@game = game # Player should know which game he's playing so he knows which board is available to him
 			@marker = marker
@@ -27,6 +33,19 @@ module TicTacToe
 	end
 
 	class HumanPlayer < Player
+		def select_position!
+			@game.print_board
+			loop do 
+				puts "Select your #{marker} position"
+				selection = gets.to_i
+				return selection if @game.free_positions.include?(position)
+				puts "Position #{selection} is not available, Try again!!"
+			end
+		end
+
+		def to_s
+			"Human"
+		end
 	end
 end
 
